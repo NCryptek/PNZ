ad6a087804c0229f391bfdb95ecbfd304444f646
NCryptek <Artur.mail.J@proton.me>
2025-01-02 21:02:58 +0100

    Map Update (without classes, because no normal constructor)

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,37 @@
 #include <iostream>
-#include <test.h>
-#include <nlohmann/json.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <fstream>
+#include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
-sf::Sprite MapSprite(int x, int y, int tile) {
+// NOTE: Some constants, size of each tile on map and the size of maps (in tiles) 
+// WARNING!: For some strange reasons (and no longer existing default sf::Sprite constructor) I had to do it with classes
+int tileWidth = 64, tileHeight = 64;
+const int MaxWidth = 4, MaxHeight = 6;
+
+// NOTE: a draw Sprite function (will be updated)
+void DrawMapSprite(int x, int y, int tile, sf::RenderWindow &window) {
     int a = 0+x;
     int b = 0+y;
     sf::Texture textu("assets/ground.png");
-
     sf::Sprite sprite(textu);
+    
+    sprite.setTextureRect(sf::IntRect({tileWidth, 0}, {tileWidth, tileHeight}));
+    if (tile == 0)
+        sprite.setTextureRect(sf::IntRect({0, 0}, {tileWidth, tileHeight}));
     sprite.setPosition({float(a), float(b)});
-    return sprite;
+    window.draw(sprite); 
 }
 
-std::vector<sf::Sprite> test (int x);
 int main()
 {
-    testowo sprawdzenie;
-    sprawdzenie.a = 4;
-    std::cout << sprawdzenie.a << std::endl;
-    sprawdzenie.zmianaA();
-   // std::cout << sprawdzenie.a << std::endl;
-
-
-
-
+    int map[MaxWidth][MaxHeight] = {0};
+    map[2][3] = 1;
 
     // NOTE: Creating map, (make it in other file later) of size of MaxWidth and MaxHeight
-    const int MaxWidth = 10, MaxHeight = 20;
-    int map [MaxWidth][MaxHeight] = {0};
 
     // NOTE: config.json is a config file (wow) that contains width, height and framerate 
     std::fstream f("./assets/config.json");
@@ -66,18 +64,19 @@ int main()
             view1.move({-MovingValue, 0});
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) && positionY > 0)
             view1.move({0, -MovingValue});
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) && positionY < MaxHeight*200)
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) && positionY < MaxHeight*64)
             view1.move({0, MovingValue});
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && positionX < MaxWidth*200)
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && positionX < MaxWidth*64)
             view1.move({MovingValue, 0});
 
         // NOTE: window.clear() will clear the screen, so if you want to draw somethig, it should be after that line, but before display()
         window.clear();
         window.setView(view1);
-
-        window.draw(MapSprite(0, 0, 0));
-
-
+        for (int i = 0; i < MaxWidth; i++)
+        {
+            for (int j = 0; j < MaxHeight; j++)
+                DrawMapSprite(i*tileWidth, j*tileHeight, map[i][j], window);
+        }
 
         window.display();
     }
